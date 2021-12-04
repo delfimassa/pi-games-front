bde04ea1f3863e3973a5abb7acce189be93361e0
Delfi <delfimassa@gmail.com>
2021-12-04 04:44:15 -0300

    se agregaron estilos a landing

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import {Link}  from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Landing.module.css";
+import logo from "../assets/img/die.png";
+// import logo from "../assets/img/alienmonster.png"
+// import logo from "../assets/img/joystick3.png"
 
 const LandingPage = () => {
-    return (
-        <div>
-            <h1>Bienvenidos a mi super pagina</h1>
-            <Link to = '/home'><button>Ingresar</button></Link>
-            
-        </div>
-    );
+  return (
+    <div className={styles.landing}>
+      <div className={styles.contenedor}>
+        <h1 className={styles.titulo}>
+          Game time <img src={logo} width="70px"></img>
+        </h1>
+        <h2 className={styles.delfi}>By Delfi Massa</h2></div>
+        <Link to="/home">
+          <button className={styles.letsgo}>Let's go!</button>
+        </Link>
+      
+    </div>
+  );
 };
 
 export default LandingPage;
